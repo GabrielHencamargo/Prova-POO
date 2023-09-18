a96e9311cdda8cc198968149428e3221311bf9a0
GabrielHencamargo <115186579+GabrielHencamargo@users.noreply.github.com>
2023-09-18 18:34:19 -0300

    Add files via upload
    1 questao

---
+++ caNome/caNome/NomeProprio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caNome
+{
+    internal class NomeProprio
+    {
+        //atributos
+        private string nomecompleto;
+        private string nome_paper;
+
+        public NomeProprio() { }
+        public NomeProprio(string _nomecompleto)
+        {
+            nomecompleto = _nomecompleto;
+        }
+        public string Nomecompleto { get => nomecompleto; set => nomecompleto = value; }
+        public string Nome_paper { get => nome_paper; set => nome_paper = value; }
+        
+        //metodos
+        public void geranomepaper()
+        {
+            if ((nomecompleto.Split(" ")).Length >= 3)
+            {
+                string primeironome = nomecompleto.Split(" ")[0];
+                string nomemeio = (nomecompleto.Split(" ")[1]);
+                string sobrenome = nomecompleto.Split(" ")[2];
+                char inicial = char.ToUpper(nomemeio[0]);
+                nome_paper =  sobrenome + "," + primeironome + " " +inicial + ".";
+            }
+            if ((nomecompleto.Split(" ")).Length < 3)
+            {
+                string primeironome = nomecompleto.Split(" ")[0];
+                string sobrenome = (nomecompleto.Split(" ")[1]);
+                nome_paper =  sobrenome + "," + primeironome;
+            }
+            
+
+            
+            
+            
+        }
+    }
+}
---
+++ caNome/caNome/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using caNome;
+
+Console.WriteLine("Digite o nome: ");
+string nome = Console.ReadLine();
+NomeProprio name = new NomeProprio(nome);
+name.geranomepaper();
+Console.WriteLine(name.Nome_paper);
